2de88a9413133f4f0398cc59e4f8064b30fddb5f
jdawg093 <49189518+jdawg093@users.noreply.github.com>
2025-03-03 17:14:10 +1100

    ON-47268 # Updated function to pass in fields instead

--- src/freshdeskService.ts
+++ src/freshdeskService.ts
@@ -52,122 +52,119 @@ export type FreshdeskDependentField = {
 export function generateFreshdeskTicketFieldDefinitions(
   fields: FreshdeskTypes.FreshdeskField[],
 ) {
-  return fields.reduce<FreshdeskFieldDefinition[]>((memo, freshdeskField) => {
-    const isCheckboxField = freshdeskField.type === checkboxFieldType
-    const isRequired =
-      freshdeskField.required_for_customers ||
-      freshdeskField.name === 'ticket_type'
-    const isTextField = textFieldTypes.includes(freshdeskField.type)
-    const isMultilineTextField = freshdeskField.type === multilineFieldType
-    const isEmailField = freshdeskField.type === emailFieldType
-    const isDateField = freshdeskField.type === dateFieldType
-    const isNumberField = numberFieldTypes.includes(freshdeskField.type)
-    const isChoiceField = choiceFieldTypes.includes(freshdeskField.type)
+  return setFreshdeskFieldOptions(fields).reduce<FreshdeskFieldDefinition[]>(
+    (memo, freshdeskField) => {
+      const isCheckboxField = freshdeskField.type === checkboxFieldType
+      const isRequired =
+        freshdeskField.required_for_customers ||
+        freshdeskField.name === 'ticket_type'
+      const isTextField = textFieldTypes.includes(freshdeskField.type)
+      const isMultilineTextField = freshdeskField.type === multilineFieldType
+      const isEmailField = freshdeskField.type === emailFieldType
+      const isDateField = freshdeskField.type === dateFieldType
+      const isNumberField = numberFieldTypes.includes(freshdeskField.type)
+      const isChoiceField = choiceFieldTypes.includes(freshdeskField.type)
 
-    const baseField = {
-      id: freshdeskField.id.toString(),
-      displayName: freshdeskField.label_for_customers,
-      isRequired,
-      freshdeskFieldName: freshdeskField.name,
-    }
-    if (isCheckboxField) {
-      memo.push({
-        type: 'BOOLEAN',
-        ...baseField,
-      })
-    } else if (isDateField) {
-      memo.push({
-        type: 'DATE',
-        ...baseField,
-      })
-    } else if (isNumberField) {
-      memo.push({
-        type: 'NUMBER',
-        ...baseField,
-      })
-    } else if (isEmailField) {
-      memo.push({
-        type: 'EMAIL',
-        ...baseField,
-      })
-    } else if (isMultilineTextField) {
-      memo.push({
-        type: 'TEXT_MULTI_LINE',
-        ...baseField,
-      })
-    } else if (isTextField) {
-      memo.push({
-        type: 'TEXT_SINGLE_LINE',
-        ...baseField,
-      })
-    } else if (
-      (freshdeskField.options || freshdeskField.choices) &&
-      isChoiceField
-    ) {
-      memo.push({
-        type: 'CHOICE_SINGLE',
-        ...baseField,
-        choices: transformFreshdeskChoicesIntoOptions(
-          freshdeskField.options,
-          freshdeskField.choices,
-        ),
-        checkIsFormElementSupported(formElementWithOptions) {
-          if (freshdeskField.type === 'nested_field') {
-            return false
-          }
+      const baseField = {
+        id: freshdeskField.id.toString(),
+        displayName: freshdeskField.label_for_customers,
+        isRequired,
+        freshdeskFieldName: freshdeskField.name,
+      }
+      if (isCheckboxField) {
+        memo.push({
+          type: 'BOOLEAN',
+          ...baseField,
+        })
+      } else if (isDateField) {
+        memo.push({
+          type: 'DATE',
+          ...baseField,
+        })
+      } else if (isNumberField) {
+        memo.push({
+          type: 'NUMBER',
+          ...baseField,
+        })
+      } else if (isEmailField) {
+        memo.push({
+          type: 'EMAIL',
+          ...baseField,
+        })
+      } else if (isMultilineTextField) {
+        memo.push({
+          type: 'TEXT_MULTI_LINE',
+          ...baseField,
+        })
+      } else if (isTextField) {
+        memo.push({
+          type: 'TEXT_SINGLE_LINE',
+          ...baseField,
+        })
+      } else if (freshdeskField.options && isChoiceField) {
+        memo.push({
+          type: 'CHOICE_SINGLE',
+          ...baseField,
+          choices: freshdeskField.options,
+          checkIsFormElementSupported(formElementWithOptions) {
+            if (freshdeskField.type === 'nested_field') {
+              return false
+            }
 
-          return (
-            formElementWithOptions.freshdeskFieldName === freshdeskField.name
-          )
-        },
-      })
-    } else if (
-      freshdeskField.type === 'nested_field' &&
-      (freshdeskField.options?.length || freshdeskField.choices)
-    ) {
-      const choices = transformFreshdeskChoicesIntoOptions(
-        freshdeskField.options,
-        freshdeskField.choices,
-      ).reduce<FreshdeskFieldCategory[]>((categoriesMemo, category) => {
-        const subCategories = category.options?.reduce<
-          FreshdeskFieldCategory['subCategories']
-        >((subCategoriesMemo, subCategory) => {
-          if (subCategory.options?.length) {
-            subCategoriesMemo.push({
-              value: subCategory.value,
-              label: subCategory.label,
-              items: subCategory.options.map(({ value, label }) => ({
-                value,
-                label,
-              })),
-            })
-          }
+            return (
+              formElementWithOptions.freshdeskFieldName === freshdeskField.name
+            )
+          },
+        })
+      } else if (
+        freshdeskField.type === 'nested_field' &&
+        freshdeskField.options?.length
+      ) {
+        const choices = freshdeskField.options.reduce<FreshdeskFieldCategory[]>(
+          (categoriesMemo, category) => {
+            const subCategories = category.options?.reduce<
+              FreshdeskFieldCategory['subCategories']
+            >((subCategoriesMemo, subCategory) => {
+              if (subCategory.options?.length) {
+                subCategoriesMemo.push({
+                  value: subCategory.value,
+                  label: subCategory.label,
+                  items: subCategory.options.map(({ value, label }) => ({
+                    value,
+                    label,
+                  })),
+                })
+              }
 
-          return subCategoriesMemo
-        }, [])
+              return subCategoriesMemo
+            }, [])
 
-        if (subCategories?.length) {
-          categoriesMemo.push({
-            value: category.value,
-            label: category.label,
-            subCategories,
+            if (subCategories?.length) {
+              categoriesMemo.push({
+                value: category.value,
+                label: category.label,
+                subCategories,
+              })
+            }
+
+            return categoriesMemo
+          },
+          [],
+        )
+
+        if (choices.length) {
+          memo.push({
+            type: 'DEPENDENT_FIELD',
+            ...baseField,
+            choices,
           })
         }
-
-        return categoriesMemo
-      }, [])
-
-      if (choices.length) {
-        memo.push({
-          type: 'DEPENDENT_FIELD',
-          ...baseField,
-          choices,
-        })
       }
-    }
 
-    return memo
-  }, [])
+      return memo
+    },
+    [],
+  )
 }
 
 /**
@@ -179,21 +176,21 @@ export function generateFreshdeskTicketFieldDefinitions(
  * @param choices
  * @returns
  */
-export function transformFreshdeskChoicesIntoOptions(
-  options: FreshdeskTypes.FreshdeskFieldOption[] | undefined,
-  choices: FreshdeskTypes.FreshdeskField['choices'],
-): FreshdeskTypes.FreshdeskFieldOption[] {
-  if (options) {
-    return options
-  }
-  if (choices) {
+export function setFreshdeskFieldOptions(
+  fields: FreshdeskTypes.FreshdeskField[],
+): FreshdeskTypes.FreshdeskField[] {
+  for (const field of fields) {
+    const choices = field.choices
+    if (!choices) {
+      continue
+    }
     if (Array.isArray(choices)) {
-      return choices.map((choice) => ({
+      field.options = choices.map((choice) => ({
         label: choice,
         value: choice,
       }))
     } else if (typeof choices === 'object') {
-      return Object.entries(choices).map(([key, value]) => {
+      field.options = Object.entries(choices).map(([key, value]) => {
         if (typeof value === 'number') {
           return {
             label: key,
@@ -204,21 +201,22 @@ export function transformFreshdeskChoicesIntoOptions(
             label: value[0],
             value: parseInt(key),
           }
-        }
-        return {
-          label: key,
-          value: key,
-          options: Object.entries(value).map(([subCategory, items]) => ({
-            label: subCategory,
-            value: subCategory,
-            options: items.map((item) => ({
-              label: item,
-              value: item,
+        } else {
+          return {
+            label: key,
+            value: key,
+            options: Object.entries(value).map(([subCategory, items]) => ({
+              label: subCategory,
+              value: subCategory,
+              options: items.map((item) => ({
+                label: item,
+                value: item,
+              })),
             })),
-          })),
+          }
         }
       })
     }
   }
-  return []
+  return fields
 }
