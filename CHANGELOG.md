6c86ef9e0247238f3aaca8a691f8e41ec281a064
David Porter <david@oneblink.io>
2025-07-10 12:24:05 +1000

    AP-4032 # fix unnecessary tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- `goodToGoService`
+
 ## [8.5.1] - 2025-07-10
 
 ### Added
--- tests/goodToGoService.test.ts
+++ tests/goodToGoService.test.ts
@@ -25,50 +25,6 @@ describe('generateGoodToGoFieldResourceDefinitions', () => {
     ])
   })
 
-  it('should map TEXT_MULTI_LINE fields correctly', () => {
-    const fields: GetFieldResponse[] = [
-      {
-        id: 'b',
-        name: 'textarea',
-        label: 'Textarea',
-        required: true,
-        dataType: 'STRING',
-        inputType: 'MULTILINE',
-      },
-    ]
-
-    expect(generateGoodToGoFieldResourceDefinitions(fields)).toEqual([
-      {
-        type: 'TEXT_MULTI_LINE',
-        id: 'b',
-        goodToGoCustomFieldName: 'textarea',
-        isRequired: true,
-        displayName: 'Textarea',
-      },
-    ])
-  })
-
-  it('should map DATE fields correctly', () => {
-    const fields: GetFieldResponse[] = [
-      {
-        id: 'c',
-        name: 'date',
-        label: 'Date',
-        dataType: 'DATE',
-      },
-    ]
-
-    expect(generateGoodToGoFieldResourceDefinitions(fields)).toEqual([
-      {
-        type: 'DATE',
-        id: 'c',
-        goodToGoCustomFieldName: 'date',
-        displayName: 'Date',
-        isRequired: false,
-      },
-    ])
-  })
-
   it('should skip fields that are not yet supported', () => {
     const fields: GetFieldResponse[] = [
       {
@@ -83,57 +39,6 @@ describe('generateGoodToGoFieldResourceDefinitions', () => {
     expect(generateGoodToGoFieldResourceDefinitions(fields)).toEqual([])
   })
 
-  it('should map a mix of fields correctly', () => {
-    const fields: GetFieldResponse[] = [
-      {
-        id: 'a',
-        name: 'text',
-        label: 'Text',
-        required: true,
-        dataType: 'STRING',
-        inputType: 'SINGLELINE',
-      },
-      {
-        id: 'b',
-        name: 'textarea',
-        label: 'Textarea',
-        required: true,
-        dataType: 'STRING',
-        inputType: 'MULTILINE',
-      },
-      {
-        id: 'c',
-        name: 'date',
-        label: 'Date',
-        dataType: 'DATE',
-      },
-    ]
-
-    expect(generateGoodToGoFieldResourceDefinitions(fields)).toEqual([
-      {
-        type: 'TEXT_SINGLE_LINE',
-        id: 'a',
-        goodToGoCustomFieldName: 'text',
-        isRequired: true,
-        displayName: 'Text',
-      },
-      {
-        type: 'TEXT_MULTI_LINE',
-        id: 'b',
-        goodToGoCustomFieldName: 'textarea',
-        displayName: 'Textarea',
-        isRequired: true,
-      },
-      {
-        type: 'DATE',
-        id: 'c',
-        goodToGoCustomFieldName: 'date',
-        displayName: 'Date',
-        isRequired: false,
-      },
-    ])
-  })
-
   it('should use field name if label is not provided', () => {
     const fields: GetFieldResponse[] = [
       {
@@ -142,18 +47,6 @@ describe('generateGoodToGoFieldResourceDefinitions', () => {
         required: true,
         dataType: 'STRING',
         inputType: 'SINGLELINE',
-      },
-      {
-        id: 'b',
-        name: 'textarea',
-        required: true,
-        dataType: 'STRING',
-        inputType: 'MULTILINE',
-      },
-      {
-        id: 'c',
-        name: 'date',
-        dataType: 'DATE',
       },
     ]
 
@@ -164,20 +57,6 @@ describe('generateGoodToGoFieldResourceDefinitions', () => {
         goodToGoCustomFieldName: 'text',
         isRequired: true,
         displayName: 'text',
-      },
-      {
-        type: 'TEXT_MULTI_LINE',
-        id: 'b',
-        goodToGoCustomFieldName: 'textarea',
-        displayName: 'textarea',
-        isRequired: true,
-      },
-      {
-        type: 'DATE',
-        id: 'c',
-        goodToGoCustomFieldName: 'date',
-        displayName: 'date',
-        isRequired: false,
       },
     ])
   })
