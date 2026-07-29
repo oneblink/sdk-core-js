70c9bb4d73b30695e6ad4a03b56be5742404757b
David Porter <david@oneblink.io>
2026-07-30 09:16:14 +1000

    AP-9816 # checkForPaymentEvent no longer returns amount

--- src/paymentService.ts
+++ src/paymentService.ts
@@ -3,11 +3,8 @@ import {
   SubmissionEventTypes,
   SubmissionTypes,
 } from '@oneblink/types'
-import { conditionalLogicService, formElementsService } from './index.js'
-import {
-  getRootElementValueById,
-  ReplaceInjectablesFormatters,
-} from './submissionService.js'
+import { conditionalLogicService } from './index.js'
+import { ReplaceInjectablesFormatters } from './submissionService.js'
 import { AddOffsetToDate, ParseDate } from './conditionalLogicService/types.js'
 
 /**
@@ -50,7 +47,6 @@ export function checkForPaymentEvent({
 }):
   | {
       paymentSubmissionEvent: SubmissionEventTypes.FormPaymentEvent
-      amount: number
     }
   | undefined {
   const paymentSubmissionEvents = definition.paymentEvents || []
@@ -78,49 +74,12 @@ export function checkForPaymentEvent({
     return
   }
   console.log(
-    'Checking if submission with payment submission event needs processing',
+    'Found payment submission event',
+    JSON.stringify(paymentSubmissionEvent, null, 2),
   )
-
-  const amountElement = formElementsService.findFormElement(
-    definition.elements,
-    (element) => element.id === paymentSubmissionEvent.configuration.elementId,
-  )
-  if (!amountElement || amountElement.type === 'page') {
-    console.log(
-      'Form has a payment submission event but the amount element does not exist, throwing error',
-    )
-    throw new Error(
-      'We could not find the configuration required to make a payment. Please contact your administrator to ensure your application configuration has been completed successfully.',
-    )
-  }
-
-  console.log('Found form element for payment submission event', amountElement)
-
-  const amount = getRootElementValueById(
-    amountElement.id,
-    definition.elements,
-    submission,
-  )
-
-  if (!amount) {
-    console.log(
-      'Form has a payment submission event but the amount has been entered as 0 or not at all, finishing as normal submission',
-    )
-    return
-  }
-
-  if (typeof amount !== 'number') {
-    console.log(
-      'Form has a payment submission event but the amount is not a number, throwing error',
-    )
-    throw new Error(
-      'The configuration required to make a payment is incorrect. Please contact your administrator to ensure your application configuration has been completed successfully.',
-    )
-  }
 
   const result = {
     paymentSubmissionEvent,
-    amount,
   }
 
   return result
